84e41c3824322d4a86475488e8c02fac7408492c
Sean Cross <sean@xobs.io>
2023-04-05 12:41:11 +0800

    blackmagic: incorporate external pbuf change
    The pbuf struct now comes from a function call rather than as a static
    value. Implement `gdb_packet_buffer()` to provide the current pbuf to
    the hostio component.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- main/gdb_main_farpatch.h
+++ main/gdb_main_farpatch.h
@@ -2,6 +2,7 @@
 #define GDB_MAIN_FARPATCH_H_
 
 #include <freertos/FreeRTOS.h>
+#include <gdb_main.h>
 
 #define EXCEPTION_NETWORK 0x40
 #define EXCEPTION_MUTEX   0x41
@@ -18,7 +19,7 @@ struct bmp_wifi_instance {
 	bool is_shutting_down;
 	uint8_t tx_buf[1024];
 	int rx_buf_index;
-	char rx_buf[1024 + 1];
+	char rx_buf[GDB_PACKET_BUFFER_SIZE + 1];
 };
 
 #endif /* GDB_MAIN_FARPATCH_H_ */
