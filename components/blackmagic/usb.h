861720ee3276ae207ba617e41d2dfd21bd0e311d
Sean Cross <sean@xobs.io>
2024-06-29 17:29:54 +0800

    blackmagic: fix nightly build by adding `usb.h`
    This file is now included when TRACESWO is enabled.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ components/blackmagic/usb.h
@@ -0,0 +1,11 @@
+#ifndef USB_H__
+#define USB_H__
+
+/* This file is included by `command.c` when TRACESWO is enabled. */
+
+/* This dummy value is only used for printing out a notification message.
+ * The value has no practical use.
+ */
+#define TRACE_ENDPOINT -1
+
+#endif /* !defined USB_H__ */
