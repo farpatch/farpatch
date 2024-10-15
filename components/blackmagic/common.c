0804b6f59920d2564aed0b7e3d5c5bfb503e8565
Sean Cross <sean@xobs.io>
2024-10-15 08:10:49 +0800

    update firmware name
    Signed-off-by: Sean Cross <sean@xobs.io>

--- components/blackmagic/common.c
+++ components/blackmagic/common.c
@@ -1,7 +1,7 @@
 #include <freertos/FreeRTOS.h>
 #include "esp_attr.h"
 
-// Insert a sleep statement at least once every 1.5 seconds in order
+// Insert a sleep statement 500 ms before the WDT timeout starts,
 // to ensure non-critical tasks can run.
 void IRAM_ATTR platform_maybe_delay(void)
 {
@@ -9,7 +9,7 @@ void IRAM_ATTR platform_maybe_delay(void)
 	static TickType_t current_sleep = 0;
 
 	current_sleep = xTaskGetTickCount();
-	if ((current_sleep - last_sleep) > pdMS_TO_TICKS(3500)) {
+	if ((current_sleep - last_sleep) > pdMS_TO_TICKS((CONFIG_ESP_TASK_WDT_TIMEOUT_S * 1000) - 500)) {
 		last_sleep = current_sleep;
 		vTaskDelay(2);
 	}
